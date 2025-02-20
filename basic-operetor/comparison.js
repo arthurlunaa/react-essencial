6940c9b48312cfca4c7d3134f5e31a528d225587
arthurlunaa <henriquearthur192@gmail.com>
2025-02-20 19:57:04 -0300

    create: essencial-react

--- basic-operetor/comparison.js
+++ basic-operetor/comparison.js
@@ -1,28 +1,38 @@
-// operador de comparação 
+// // operador de comparação 
 
-// igualdade 
-console.log(5 == '5')
+// // igualdade 
+// console.log(5 == '5')
 
-// igualdade estrito 
-console.log (5==='5')
+// // igualdade estrito 
+// console.log (5==='5')
 
-// diferente 
-console.log( 5!= '5')
-// estrito
-console.log( 5!== '5')
+// // diferente 
+// console.log( 5!= '5')
+// // estrito
+// console.log( 5!== '5')
 
-// maior 
+// // maior 
 
-console.log(10 > 5)
+// console.log(10 > 5)
 
-// maior ou igual
-console.log (10 >= 5)
+// // maior ou igual
+// console.log (10 >= 5)
 
-// menor
-console.log(10 < 5)
-// menor igual 
-console.log (10<= 5)
+// // menor
+// console.log(10 < 5)
+// // menor igual 
+// console.log (10<= 5)
 
-let podeBeber = 18 >= 21
-if podeBeber( 21 )
+// let podeBeber = 18 >= 21
+// if podeBeber( 21 )
+// // console.log(podeBeber)
+
+let idade = 18
+
+let dinheiro = tr
+ue
+let carro = false
+
+let podeBeber = (idade >=18 && dinheiro ) && (carro)
+
 console.log(podeBeber)
--- basic-operetor/if-else.js
+++ basic-operetor/if-else.js
@@ -1,7 +1,11 @@
-const nota =6
+const nota =2
 
 if (nota ===10){
     return console.log('passou por excelencia')
-}else if (nota <=9 && nota >=7){
-    return console.log('passou garoto(a)')
+}else if (nota <=9 && nota >=8){
+    return console.log('passou sem sustos garoto(a)')
+}else if (nota >=5 && nota <=7 ){
+    return console.log ('aprovado mas tem melhorar as notas')
+}else if (nota <=3){
+    return console.log('reprovado')
 }
