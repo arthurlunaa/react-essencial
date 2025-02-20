4261b7df930ecbc64cf7d57882d42e5ed0be6892
arthurlunaa <henriquearthur192@gmail.com>
2025-02-19 21:36:27 -0300

    create: essencial-react

---
+++ basic-operetor/assigments.js
@@ -0,0 +1,15 @@
+// operador de atribuição 
+
+// simples 
+let x = 10
+console.log(x)
+// adição
+
+x+=5
+console.log(x)
+
+// subtração
+// x =x -8 
+x -=8
+console.log(x)
+// modando linha por linha 
