84cf2f2bcd4a52a0c8381ffe509ef37360c2df15
arthurlunaa <henriquearthur192@gmail.com>
2025-02-19 21:23:49 -0300

    create: essencial-react

---
+++ basic-operetor/logical-operetor.js
@@ -0,0 +1,22 @@
+// operador aritmeticos 
+
+// aDIÇÃO
+
+let soma = 2+2
+console.log(soma)
+
+// subtração
+console.log(diferenca)
+let diferenca = 10 - 3
+
+// mulpicação 
+let produto = 10* 5
+console.log(produto)
+
+// divisão 
+let quaociente = 20/4
+console.log(quaociente)
+
+// modulo
+let resto = 10 % 3
+console.log(resto)
