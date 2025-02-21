a34cdf5ac03a3424e94b79bcea6a7e666628e7a0
arthurlunaa <henriquearthur192@gmail.com>
2025-02-20 21:35:38 -0300

    create: essencial-react

--- condition/if.js
+++ condition/if.js
@@ -1,14 +1,13 @@
-let nota = 6
-
-if (nota = 10) {
-    return console.log(`nota maxima , parabens!`)
-}
-else if (nota >= 7) {
-    return console.log('aprovado com sucesso!')
-} else if (nota >= 4.5) {
-    return console.log(`sua nota foi ${nota}, venha na escola agendar sua recuperação `)
-}
-else {
-    return console.log(`sua nota foi ${nota}, por isso foi repravodo `)
-}
+// let nota ;
+// if (nota = 10) {
+//     return console.log(`nota maxima , parabens!`)
+// }
+// else if (nota >= 7) {
+//     return console.log('aprovado com sucesso!')
+// } else if (nota >= 4.5) {
+//     return console.log(`sua nota foi ${nota}, venha na escola agendar sua recuperação `)
+// }
+// else {
+//     return console.log(`sua nota foi ${nota}, por isso foi repravodo `)
+// }
 
---
+++ condition/switch.js
@@ -0,0 +1,9 @@
+let nota = 7
+switch (true) {
+    case (nota ===10) :
+      console.log(`nota maxima foi de &{nota}, parabens `)
+        break
+        default:
+        return console.log(`sua nota foi de ${nota}, venha na escola agendar sua recuperação `)
+      
+}
